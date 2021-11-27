da7fa1a87690fb0cfae50c89c73553c185c205e8
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2021-11-27 17:15:41 +0800

    updated clang-format so that array elements are aligned with spaces, not tabs (they look better that way on github)

--- weddingcoins.cpp
+++ weddingcoins.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main() {
 	long long n, i = 0, notes = 0;
 	int vals[13] = {1000000, 500000, 100000, 50000, 10000, 5000, 1000,
-									500,		 100,		 50,		 10,		5,		 1};
+									500,     100,    50,     10,    5,     1};
 	cin >> n;
 	while (n > 0) {
 		notes += (long long) n / vals[i];
 		n %= vals[i];
-		++i;
+		i++;
 	}
 	cout << notes << '\n';
 }
