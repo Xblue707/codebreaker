01db8968a592874ca15ba6548448ceb8cd54179c
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2021-12-06 17:56:05 +0800

    updated variable names

--- carpark.cpp
+++ carpark.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	int n = 0, s = 0, temp, answer = 0, lol = 0, counter = 0, k = 0, c = 0;
+	int n = 0, s = 0, temp, ans = 0, lol = 0, count = 0, k = 0, c = 0;
 	priority_queue<int, vector<int>, greater<int>> gaps;
 
 	cin >> n >> s;
@@ -22,25 +22,25 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		if (cars[i] == 1) {
 			c = 1;
-			answer++;
-			if (counter > 0) {
-				gaps.push(counter);
-				counter = 0;
+			ans++;
+			if (count > 0) {
+				gaps.push(count);
+				count = 0;
 			}
 		}
 		else if (cars[i] == 0) {
 			k += c;
 			c = 0;
-			counter++;
+			count++;
 		}
 	}
 
 	if (cars[n - 1] == 1) { k++; }
 
 	for (int i = s; i < k; i++) {
-		answer += gaps.top();
+		ans += gaps.top();
 		gaps.pop();
 	}
 
-	cout << answer;
+	cout << ans;
 }
