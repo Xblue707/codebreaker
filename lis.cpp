fa59c929b7a620b824bfc726b8587f8ed4f1cbab
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-01-28 18:11:30 +0800

    updated *_ex file names

--- lis.cpp
+++ lis.cpp
@@ -1,30 +1,24 @@
-#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int lis(int arr[], int n) {
-	int lis[n];
-
-	lis[0] = 1;
-
-	for (int i = 1; i < n; i++) {
-		lis[i] = 1;
-		for (int j = 0; j < i; j++)
-			if (arr[i] > arr[j] && lis[i] < lis[j] + 1) lis[i] = lis[j] + 1;
-	}
-
-	return *max_element(lis, lis + n);
-}
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
 	int n;
 	cin >> n;
-	int arr[n];
-	for (int i = 0; i < n; i++) cin >> arr[i];
-	cout << lis(arr, n);
 
-	return 0;
+	vector<int> nums = vector<int>(n);
+	for (int i = 0; i < n; i++) cin >> nums[i];
+
+	int maxsum = 0;
+	vector<int> lis;
+	for (int x : nums) {
+		auto it = lower_bound(lis.begin(), lis.end(), x);
+		const int idx = it - lis.begin();
+		if (it == lis.end()) lis.push_back(x);
+		else *it = x;
+
+		if (idx == maxsum) maxsum++;
+	}
+
+	cout << maxsum;
 }
