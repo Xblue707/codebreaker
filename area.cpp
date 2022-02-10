cf293d68d2ceefc49633039d9b9101c406f6e509
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-02-11 06:44:55 +0800

    chore: format

--- area.cpp
+++ area.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main() {
 	long n, total = 0;
 	cin >> n;
+
 	for (int i = 0; i < n; i++) {
 		int a, b;
 		cin >> a >> b;
-
 		long area = a * b;
 		total += area;
 	}
+
 	cout << total;
 }
