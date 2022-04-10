2ad289ab18a8e0daad62dffa5be3cd4c432fa51d
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-04-10 18:08:13 +0800

    concise

--- solutions/7up.cpp
+++ solutions/7up.cpp
@@ -7,8 +7,7 @@ int main() {
 	int n, k, x;
 	cin >> n >> k >> x;
 	for (int i = 0; i < k; i++) {
-		string s = to_string(x);
-		if (x % 7 == 0 || s.find('7') != string::npos) cout << "UP!\n";
+		if (x % 7 == 0 || to_string(x).find('7') != string::npos) cout << "UP!\n";
 		else cout << x << '\n';
 		x += n;
 	}
