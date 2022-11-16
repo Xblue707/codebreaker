d0a47857c2ad9999713c451c9d5c3302333f6c49
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-11-16 12:54:12 +0800

    Create peakfinding.cc

---
+++ solutions/peakfinding.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+#define rep(i, s, e) for (ll i = (s); i < (e); i++)
+#define per(i, s, e) for (ll i = (s); i > (e); i--)
+#define f0r(i, n) for (ll i = 0; i < (n); i++)
+#define r0f(i, n) for (ll i = (n); i >= 0; i--)
+#define far(i, v) for (auto i : (v))
+#define setup ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+template<typename T> inline void in(T& inVar) { cin >> inVar; }
+template<typename T, typename... S> inline void in(T& inVar, S&... args) { cin >> inVar; in(args ...); }
+template<typename T> inline void out(T outVar) { cout << outVar << '\n'; }
+template<typename T, typename ...S> inline void out(T outVar, S... args) { cout << outVar << ' '; out(args...); }
+typedef vector<ll> vi;
+typedef pair<ll, ll> pi;
+typedef vector<pi> vpi;
+#define vc vector
+#define dq deque
+#define qu queue
+#define pq priority_queue
+#define st stack
+#define ma map
+#define fi first
+#define se second
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (ll)(c.size())
+#define pb push_back
+#define mp make_pair
+/* do not delete */
+
+int main() {
+	setup;
+	int t; in(t);
+	int a[t];
+	f0r(i,t) in(a[i]);
+	out(*max_element(a,a+t));
+	return 0;
+}
