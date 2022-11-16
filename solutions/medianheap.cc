1dcde534833d420dd39a7bee419fc9efd2c45354
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-11-16 12:51:36 +0800

    Create medianheap.cc

---
+++ solutions/medianheap.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<
+    int,
+    null_type,
+    less_equal<int>,
+    rb_tree_tag,
+    tree_order_statistics_node_update> ordered_set;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ordered_set o;
+	
+	int cnt = 0; int n; cin >> n;
+	while (n--) {
+		string s;
+		cin >> s;
+		if (s == "PUSH") {
+			int x; cin >> x;
+			o.insert(x);
+			cnt++;
+		}
+		else {
+			if (cnt & 1) {
+				ordered_set::iterator it = o.find_by_order(cnt / 2);
+				o.erase(it);
+			}
+			else {
+				ordered_set::iterator it = o.find_by_order(cnt / 2 - 1);
+				o.erase(it);
+			}
+			cnt--;
+		}
+	}
+	for (auto j : o) cout << j << ' '; cout << '\n';
+}
