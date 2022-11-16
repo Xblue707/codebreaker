eabe5290b99fc9e7a7068f5150d261e282c1bf36
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-11-16 12:55:20 +0800

    Create potatotree.cc

---
+++ solutions/potatotree.cc
@@ -0,0 +1,66 @@
+// potatotree
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+#define rep(i, s, e) for (ll i = (s); i < (e); i++)
+#define per(i, s, e) for (ll i = (s); i > (e); i--)
+#define f0r(i, n) for (ll i = 0; i < (n); i++)
+#define r0f(i, n) for (ll i = (n); i >= 0; i--)
+#define far(i, v) for (auto i : (v))
+#define setup                       \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr);
+template <typename T>
+inline void in(T& inVar) {
+  cin >> inVar;
+}
+template <typename T, typename... S>
+inline void in(T& inVar, S&... args) {
+  cin >> inVar;
+  in(args...);
+}
+template <typename T>
+inline void out(T outVar) {
+  cout << outVar << '\n';
+}
+template <typename T, typename... S>
+inline void out(T outVar, S... args) {
+  cout << outVar << ' ';
+  out(args...);
+}
+typedef vector<ll> vi;
+typedef pair<ll, ll> pi;
+typedef vector<pi> vpi;
+#define vc vector
+#define dq deque
+#define qu queue
+#define pq priority_queue
+#define st stack
+#define ma map
+#define fi first
+#define se second
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (ll)(c.size())
+#define pb push_back
+#define mp make_pair
+int main() {
+  setup;
+  ll n, q;
+  in(n, q);
+  set<int> s;
+  ll ans[500005];
+  f0r(i, n) {
+    ll x;
+    in(x);
+    s.insert(x);
+    ans[i] = sz(s);
+  }
+  f0r(i, q) {
+    ll j;
+    in(j);
+    cout << ans[j - 1] << ' ';
+  }
+}
