a163a8184e3c1f0decc9c87364e7fe9b3631aef8
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-11-28 22:09:30 +0800

    Create lps.cc

---
+++ solutions/lps.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+#define rep(x, start, end)                                                   \
+	for (auto x = (start) - ((start) > (end)); x != (end) - ((start) > (end)); \
+			 ((start) < (end) ? x++ : x--))
+#define rap(i, v) for (auto i : (v))
+#define setup                       \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(nullptr);                 \
+	cout.tie(nullptr);
+#define nl '\n'
+#define pr pair
+#define vc vector
+#define dq deque
+#define qu queue
+#define pq priority_queue
+#define mp map
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+#define fi first
+#define se second
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (ll)(c.size())
+#define pub push_back
+#define pob pop_back
+#define mpr make_pair
+#define eb emplace_back
+#define debug(x) cerr << #x << ": " << x << '\n';
+
+int dp[5005][5005];
+string a, b;
+
+int lcs(int i, int j) {
+	if (i < 0 || j < 0) return 0;
+	if (dp[i][j] >= 0) return dp[i][j];
+	if (a.at(i) == b.at(j))
+		dp[i][j] = lcs(i - 1, j - 1) + 1;
+	else
+		dp[i][j] = max(lcs(i, j - 1), lcs(i - 1, j));
+	return dp[i][j];
+}
+
+int main() {
+	setup;
+	memset(dp, -1, sizeof(dp));
+	cin >> b;
+	a = b;
+	reverse(b.begin(), b.end());
+	cout << lcs(a.length() - 1, b.length() - 1);
+}
