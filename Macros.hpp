41224e984a88b84e2e282f1404d507e8f4e5f6f7
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-11-29 15:18:38 +0800

    Update Macros.hpp

--- Macros.hpp
+++ Macros.hpp
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef long int li;
+typedef long long int ll;
 typedef long double ld;
-typedef unsigned long long ull;
-#define rep(x, start, end)                                                   \
-	for (auto x = (start) - ((start) > (end)); x != (end) - ((start) > (end)); \
-			 ((start) < (end) ? x++ : x--))
-#define rap(i, v) for (auto i : (v))
-#define setup                       \
-	ios_base::sync_with_stdio(false); \
-	cin.tie(nullptr);                 \
-	cout.tie(nullptr);
-#define nl '\n'
+typedef unsigned long long int ull;
 typedef pair<int, int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
+#define rep(x, start, end)                                                   \
+	for (auto x = (start) - ((start) > (end)); x != (end) - ((start) > (end)); \
+			 ((start) < (end) ? x++ : x--))
+#define each(i, v) for (auto i : (v))
+#define fast                        \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);
+#define nl '\n'
 #define pr pair
 #define vc vector
 #define dq deque
 #define qu queue
 #define pq priority_queue
-#define ma map
+#define mp map
 #define fi first
 #define se second
 #define all(c) (c).begin(), (c).end()
 #define sz(c) (ll)(c.size())
 #define pub push_back
 #define pob pop_back
-#define mp make_pair
-#define eb emplace_back
-#define debug(x) cerr << #x << ": " << x << '\n';
+#define mpr make_pair
 mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
