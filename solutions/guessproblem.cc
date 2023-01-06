19c66f8e78a7695b761fc60d6380fb101e55deea
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2023-01-06 17:23:12 +0800

    Create guessproblem.cc

---
+++ solutions/guessproblem.cc
@@ -0,0 +1,44 @@
+// guessproblem
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int T;
+	cin >> T;
+	switch (T) {
+		case 1:
+			cout << "helloworld";
+			break;
+		case 2:
+			cout << "prisoners";
+			break;
+		case 3:
+			cout << "wheelofmisfortune";
+			break;
+		case 4:
+			cout << "industriouslibrarian";
+			break;
+		case 5:
+			cout << "doll";
+			break;
+		case 6:
+			cout << "lvm";
+			break;
+		case 7:
+			cout << "prefixsums";
+			break;
+		case 8:
+			cout << "werewolf";
+			break;
+		case 9:
+			cout << "secret";
+			break;
+		case 10:
+			cout << "rangedeleting";
+			break;
+		case 0:
+			cout << "pancake";
+			break;
+	}
+}
