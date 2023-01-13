a7378fea68cb111a84f357d289bea4728d3136f5
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2023-01-13 15:51:03 +0800

    Create homeworkextension.cc

---
+++ solutions/homeworkextension.cc
@@ -0,0 +1,46 @@
+// homeworkextension
+#include <bits/stdc++.h>
+using namespace std;
+typedef long int li;
+typedef long long int ll;
+typedef long double ld;
+typedef unsigned long long int ull;
+typedef pair<int, int> ii;
+typedef pair<pair<int, int>, int> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+#define rep(x, start, end) \
+	for (auto x = (start) - ((start) > (end)); x != (end) - ((start) > (end)); \
+			 ((start) < (end) ? x++ : x--))
+#define each(i, v) for (auto i : (v))
+#define fast() \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL)
+#define nl '\n'
+#define pr pair
+#define vc vector
+#define dq deque
+#define qu queue
+#define pq priority_queue
+#define mp map
+#define fi first
+#define se second
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (ll)(c.size())
+#define pub push_back
+#define pob pop_back
+#define mpr make_pair
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int main() {
+	fast();
+	ll n, k, cnt = 0;
+	cin >> n >> k;
+	ll a[n], b[n];
+	rep(i, 0, n) cin >> a[i];
+	rep(i, 0, n) cin >> b[i];
+	rep(i, 0, n) {
+		if (b[i] >= k) cnt += a[i];
+	}
+	cout << cnt << nl;
+}
